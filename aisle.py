5b3437c6d2b7627cfe470c5d3b1e6b860bfa0c5f
mollusk <silvernode@gmail.com>
2020-12-06 09:59:43 -0700

    Init code

---
+++ aisle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import math
+
+
+def aisle_count(aisleName):
+    #aisleTotal = 0
+    final = []
+    while True:
+        
+        input_total = input("How many cases are on the aisle?: ")
+
+        if input_total == "".join:
+            print("No input given")
+        else:
+            print(input_total)
+            return input_total
+        #if math.isnan(input_total):
+           # print("Please enter a number")
+        
+        
+    
+
+
+
+aisle_count("17")
