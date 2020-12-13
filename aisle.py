f4d84acd5f9b512b945b10873e14176b5a2659a8
mollusk <silvernode@gmail.com>
2020-12-12 21:38:59 -0700

    write count to file

--- aisle.py
+++ aisle.py
@@ -45,18 +45,44 @@ def change_count():
         print("Not a option")
         
 def count_all():
+    file = open('asile-count-12-12-2020.txt', 'w+')
+
     a17 = aisle_count("17")
+    file.write(f"17 = {a17}\n")
+
     a16 = aisle_count("16")
+    file.write(f"16 = {a16}\n")
+
     a15 = aisle_count("15")
+    file.write(f"15 = {a15}\n")
+
     a14 = aisle_count("14")
+    file.write(f"14 = {a14}\n")
+
     a13 = aisle_count("13")
+    file.write(f"13 = {a13}\n")
+
     a12 = aisle_count("12")
+    file.write(f"12 = {a12}\n")
+
     a9 = aisle_count("9")
+    file.write(f"9 = {a9}\n")
+
     a8 = aisle_count("8")
+    file.write(f"8 = {a8}\n")
+
     a7 = aisle_count("7")
+    file.write(f"7 = {a7}\n")
+
     a6 = aisle_count("6")
+    file.write(f"6 = {a6}\n")
+
+    
 
     loadTotal = a17 + a16 + a15 + a14 + a13 + a12 + a9 + a8 + a7 + a6
+    file.write(f"Total = {loadTotal}\n")
+
+    file.close()
 
     return loadTotal
 
@@ -71,7 +97,6 @@ while True:
 
     if choice == "1":
         print("Total cases: ", count_all())
-
     elif choice == "2":
         change_count()
     elif choice == "q":
