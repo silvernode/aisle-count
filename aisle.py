0c009269d0b3098cc21ea347d52f241d81e3d71e
mollusk <silvernode@gmail.com>
2020-12-21 22:43:56 -0700

    refactor, calculate per person

--- aisle.py
+++ aisle.py
@@ -11,25 +11,25 @@ class aisle:
 
     
 
-    def count(self):
-        #invalidChars = set(string.punctuation.replace("_", ""))
-        while keepGoing == True:
-            
-            input_total = input(f"How many cases are on the aisle {self.name}?: ")
-            
+def check_number(message):
+#invalidChars = set(string.punctuation.replace("_", ""))
+    while keepGoing == True:
+
+        enter_number = input(message)
+
+        try:
+            cases = int(enter_number)
+            #print("Input is an integer number.")
+            #print("Input number is: ", val)
+            return cases
+        except ValueError:
             try:
-                self.cases = int(input_total)
-                #print("Input is an integer number.")
+                cases = float(enter_number)
+                #print("Input is an float number.")
                 #print("Input number is: ", val)
-                return self.cases
+                return cases
             except ValueError:
-                try:
-                    float(input_total)
-                    #print("Input is an float number.")
-                    #print("Input number is: ", val)
-                    return self.cases
-                except ValueError:
-                    print("This is not a number. Please enter a valid number")
+                print("This is not a number. Please enter a valid number")
 
         #if input_total == "": 
         #    print("No input given")
@@ -55,38 +55,38 @@ a6 = aisle("6", 0)
 def count_all():
     
     timestr = time.strftime("%Y%m%d-%H%M%S")
-    fileName = f"aisle-count-{timestr}.txt"
+    fileName = f"aisle-check_number-{timestr}.txt"
     file = open(fileName, 'w+')
 
     
-    a17.cases = a17.count()
+    a17.cases = check_number("Total cases for aisle 17: ")
     file.write(f"17 = {a17.cases}\n")
 
-    a16.cases = a16.count()
+    a16.cases = check_number(f"Total cases for aisle 16: ")
     file.write(f"16 = {a16.cases}\n")
 
-    a15.cases = a15.count()
+    a15.cases = check_number("Total cases for aisle 15:")
     file.write(f"15 = {a15.cases}\n")
 
-    a14.cases = a14.count()
+    a14.cases = check_number("Total cases for aisle 14: ")
     file.write(f"14 = {a14.cases}\n")
 
-    a13.cases = a13.count()
+    a13.cases = check_number("Total cases for aisle 13: ")
     file.write(f"13 = {a13.cases}\n")
 
-    a12.cases = a12.count()
+    a12.cases = check_number("Total cases for aisle 12: ")
     file.write(f"12 = {a12.cases}\n")
 
-    a9.cases = a9.count()
+    a9.cases = check_number("Total cases for aisle 9: ")
     file.write(f"9 = {a9.cases}\n")
 
-    a8.cases = a8.count()
+    a8.cases = check_number("Total cases for aisle 8: ")
     file.write(f"8 = {a8.cases}\n")
 
-    a7.cases = a7.count()
+    a7.cases = check_number("Total cases for aisle 7: ")
     file.write(f"7 = {a7.cases}\n")
 
-    a6.cases = a6.count()
+    a6.cases = check_number("Total cases for aisle 6: ")
     file.write(f"6 = {a6.cases}\n")
 
     
@@ -98,17 +98,24 @@ def count_all():
 
     return loadTotal
 
+def div_ppl():
+    #aisleTrack = aisle("0", 0)
+    ppl = check_number("How many people are working?: ")
+
+    return ppl
 
 
 while keepGoing == True:
     print()
-    print("1) Start Count")
+    print("1) Start check_number")
     print()
     choice = input("Choose option: ")
 
     if choice == "1":
         totalCases = count_all()
         print("Total cases: ", totalCases)
+        pplDiv = div_ppl() / totalCases
+        print(pplDiv)
         
     elif choice == "q":
         keepGoing = False
